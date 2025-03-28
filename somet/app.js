efa8b3595744b5440d875e84152ced53d5abb4e5
rjanupam <janupam1704@gmail.com>
2025-03-28 17:18:58 +0530

    frontend path added

--- somet/app.js
+++ somet/app.js
@@ -17,7 +17,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
