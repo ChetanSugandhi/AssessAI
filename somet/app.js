261b489ba430bc8483ffe0a6a5e8818c9c377ba2
rjanupam <janupam1704@gmail.com>
2025-03-28 17:26:58 +0530

    cors fix

--- somet/app.js
+++ somet/app.js
@@ -17,7 +17,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
