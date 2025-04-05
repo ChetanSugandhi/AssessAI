fb8aa33ee86f48d3e65c0c70cd6cbfca52127543
rjanupam <janupam1704@gmail.com>
2025-04-05 23:43:56 +0530

    for dev env, proxy setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy:
+      mode === "development"
+        ? {
+            "/api": {
+              target: "http://localhost:7777",
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+          }
+        : undefined,
+  },
+});
